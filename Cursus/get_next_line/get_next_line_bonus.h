13ce189463236a92ec02bcd67a75efbb9922e60a
Davop96 <davbp@proton.me>
2024-05-13 11:18:27 +0200

    Updated

---
+++ Cursus/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbohoyo- <dbohoyo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 15:55:48 by dbohoyo-          #+#    #+#             */
+/*   Updated: 2024/05/10 12:57:18 by dbohoyo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define MAX_FDS 10
+# define SUCCESS 0
+# define ERROR -1
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
+char			*get_next_line(int fd);
+char			*ft_strdup(const char *s1);
+int				ft_strlen(const char *c);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+void			fill_str(char *res, char *s1, char *s2);
+
+#endif
